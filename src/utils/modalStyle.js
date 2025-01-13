1a91fc6a7f167a981b5abe70e98949c79becea06
Javigsz <javiguerrerosanchez@gmail.com>
2025-01-13 15:13:32 +0100

    changed modal modalStyle

--- src/utils/modalStyle.js
+++ src/utils/modalStyle.js
@@ -10,7 +10,7 @@ export const customStyles = {
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
     width: '500px',
-    maxHeight: '50svh',
+    maxHeight: '70svh',
     backgroundColor: '#0f0e17'
   }
 }
